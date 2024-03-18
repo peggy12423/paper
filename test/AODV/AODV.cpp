03d320e098d0eca5446f10aee26a848b475e4406
peggy12423 <peggy12423@gmail.com>
2024-03-18 18:43:51 +0800

    0318_AODV_NRCA_E-DSR

--- test/AODV/AODV.cpp
+++ test/AODV/AODV.cpp
@@ -5,45 +5,37 @@
 #include <queue>     
 
 #define SINKBUFFER 10000000
-#define trans_dis 60 //m 80幾乎可以確定他傳的到sink(AODV)
-
 #define MAX_energy 6480//j (1.5V*600mA*3600sec*2 = 6480j) 2*3號電池
 #define SINKID 2000
 #define SINK_X 400
 #define SINK_Y 0
-#define SINK_BUFFER_SIZE 5000000
-#define NODE_BUFFER1 100 //0~49 一般CH接收CM用 node_buffer 40Kbytes (200格) 改了這個參數 下面的bomb也要改
-#define NODE_BUFFER2 200 //50~100 特別的傳輸用
 
 #define R 0.25 //壓縮率 設1則沒有壓縮
 #define type3f 360 //常規sensing frequency
 #define type4f 480
 #define type5f 600
 #define CHf 120 //CH trans frequency
-#define freq_change_switch 0 //0關 1開 是否要使資料量突然暴增的開關
+#define freq_change_switch 1 //0關 1開 是否要使資料量突然暴增的開關
 #define b_t 10800 //大T 每多少秒開一次 小T 每一次開多少秒
 #define s_t 1800
 #define bomb_f3 45 //爆炸sensing frequency
 #define bomb_f4 60
 #define bomb_f5 90
-
-#define ProbeEnergy 0.03 //j (8*200bit*1.5V*25mA*0.5mS = 0.00375j*8 = 0.03j) !
-/*(查到的論文:bit*50nj+bit*distance(m)平方*100nj)*/
+/**/
+#define ProbeEnergy 0.03 //j (8*200bit*1.5V*25mA*0.5mS = 0.00375j*8 = 0.03j) !/*(查到的論文:bit*50nj+bit*distance(m)平方*100nj)*/
 #define TransmitEnergy 0.00008 //0.000000001*50*200*8 j (50nj/bit by冠中) bit * 8 = byte !(是這樣算嗎)
 #define AmplifierEnergy 0.00016 //100*200*0.000000001*8 j/distance^2
 #define ReceiveEnergy 0.00008 //0.000000001*50*200j*8 (50nj/bit by冠中)
 #define Package_size 200 //bytes 
 #define node_buffer 20 //Kbytes (100格)
-
+#define trans_dis 60 //m 80幾乎可以確定他傳的到sink(AODV)
 
 /*變動實驗參數設定*/
 #define round_number 10
-#define E_NUM 200
+#define E_NUM 1000
 
 using namespace std;
-
-int S_NUM = 1000;
-
+int S_NUM = 600;
 struct P
 {
 	int src;
@@ -84,6 +76,14 @@ double macdrop(0);
 double total(0);
 int R2, R3, R4;
 
+void print_energy()
+{
+	for (int i = 0; i < S_NUM; i++)
+	{
+		fout << "node" << ns[i].id << " x : " << ns[i].x << " y : " << ns[i].y << " type : " << ns[i].type << " energy : " << ns[i].energy << " visited = " << ns[i].visited << endl;
+	}
+	fout << "--------------------------------------------------\n";
+}
 double distance(int a, int b)
 {
 	if (b != SINKID)
@@ -97,55 +97,54 @@ double distance(int a, int b)
 }
 
 void node_deployed(){
-	R2 = S_NUM * 0.25;
-	R3 = S_NUM * 0.5;
-	R4 = S_NUM * 0.75;
-	
-	int i = 0;
-	for (i; i < R2; i++)
-	{
-		ns[i].id = i;
-		ns[i].x = rand() % 200 + 1;
-		ns[i].y = rand() % 200 + 1;
-		ns[i].CH = i;
-		ns[i].type = rand() % 3 + 3;//3 4 5
-		ns[i].energy = MAX_energy;
-		ns[i].dtc = distance(i, SINKID);/*距離區SINK*/
-		ns[i].region1 = 1;
-	}
-	for (i; i < R3; i++)
-	{
-		ns[i].id = i;
-		ns[i].x = rand() % 200 + 201;
-		ns[i].y = rand() % 200 + 1;
-		ns[i].CH = i;
-		ns[i].type = rand() % 3 + 3;
-		ns[i].energy = MAX_energy;
-		ns[i].dtc = distance(i, SINKID); /*距離sink*/
-		ns[i].region1 = 2;
-	}
-	for (i; i < R4; i++)
-	{
-		ns[i].id = i;
-		ns[i].x = rand() % 200 + 1;
-		ns[i].y = rand() % 200 + 201;
-		ns[i].CH = i;
-		ns[i].type = rand() % 3 + 3;
-		ns[i].energy = MAX_energy;
-		ns[i].dtc = distance(i, SINKID);/*距離區SINKID*/
-		ns[i].region1 = 3;
-	}
-	for (i; i < S_NUM; i++)
-	{
-		ns[i].id = i;
-		ns[i].x = rand() % 200 + 201;
-		ns[i].y = rand() % 200 + 201;
-		ns[i].CH = i;
-		ns[i].type = rand() % 3 + 3;
-		ns[i].energy = MAX_energy;
-		ns[i].dtc = distance(i, SINKID);/*距離區SINKID*/
-		ns[i].region1 = 4;
-	}
+    R2 = S_NUM * 0.25;
+    R3 = S_NUM * 0.5;
+    R4 = S_NUM * 0.75;
+    int i = 0;
+    for (i; i < R2; i++)
+    {
+        ns[i].id = i;
+        ns[i].x = rand() % 200 + 1;
+        ns[i].y = rand() % 200 + 1;
+        ns[i].type = rand() % 3 + 3;//3 4 5
+        ns[i].energy = MAX_energy;
+        ns[i].visited = 0;
+        ns[i].non = 0;
+        ns[i].region1 = 1;
+    }
+    for (i; i < R3; i++)
+    {
+        ns[i].id = i;
+        ns[i].x = rand() % 200 + 201;
+        ns[i].y = rand() % 200 + 1;
+        ns[i].type = rand() % 3 + 3;
+        ns[i].energy = MAX_energy;
+        ns[i].visited = 0;
+        ns[i].non = 0;
+        ns[i].region1 = 2;
+    }
+    for (i; i < R4; i++)
+    {
+        ns[i].id = i;
+        ns[i].x = rand() % 200 + 1;
+        ns[i].y = rand() % 200 + 201;
+        ns[i].type = rand() % 3 + 3;
+        ns[i].energy = MAX_energy;
+        ns[i].visited = 0;
+        ns[i].non = 0;
+        ns[i].region1 = 3;
+    }
+    for (i; i < S_NUM; i++)
+    {
+        ns[i].id = i;
+        ns[i].x = rand() % 200 + 201;
+        ns[i].y = rand() % 200 + 201;
+        ns[i].type = rand() % 3 + 3;
+        ns[i].energy = MAX_energy;
+        ns[i].visited = 0;
+        ns[i].non = 0;
+        ns[i].region1 = 4;
+    }
 }
 
 void special_node_deployed(){
@@ -153,50 +152,50 @@ void special_node_deployed(){
 	R3 = S_NUM * 0.5;  //region 2
 	R4 = S_NUM * 0.6;  //region 3 
 	int i = 0;
-	for (i; i < R2; i++)
-	{
-		ns[i].id = i;
-		ns[i].x = rand() % 200 + 1;
-		ns[i].y = rand() % 200 + 1;
-		ns[i].CH = i;
-		ns[i].type = rand() % 3 + 3;//3 4 5
-		ns[i].energy = MAX_energy;
-		ns[i].dtc = distance(i, SINKID);/*距離區SINK*/
-		ns[i].region1 = 1;
-	}
-	for (i; i < R3; i++)
-	{
-		ns[i].id = i;
-		ns[i].x = rand() % 200 + 201;
-		ns[i].y = rand() % 200 + 1;
-		ns[i].CH = i;
-		ns[i].type = rand() % 3 + 3;
-		ns[i].energy = MAX_energy;
-		ns[i].dtc = distance(i, SINKID); /*距離sink*/
-		ns[i].region1 = 2;
-	}
-	for (i; i < R4; i++)
-	{
-		ns[i].id = i;
-		ns[i].x = rand() % 200 + 1;
-		ns[i].y = rand() % 200 + 201;
-		ns[i].CH = i;
-		ns[i].type = rand() % 3 + 3;
-		ns[i].energy = MAX_energy;
-		ns[i].dtc = distance(i, SINKID);/*距離區SINKID*/
-		ns[i].region1 = 3;
-	}
-	for (i; i < S_NUM; i++)
-	{
-		ns[i].id = i;
-		ns[i].x = rand() % 200 + 201;
-		ns[i].y = rand() % 200 + 201;
-		ns[i].CH = i;
-		ns[i].type = rand() % 3 + 3;
-		ns[i].energy = MAX_energy;
-		ns[i].dtc = distance(i, SINKID);/*距離區SINKID*/
-		ns[i].region1 = 4;
-	}
+    for (i; i < R2; i++)
+    {
+        ns[i].id = i;
+        ns[i].x = rand() % 200 + 1;
+        ns[i].y = rand() % 200 + 1;
+        ns[i].type = rand() % 3 + 3;//3 4 5
+        ns[i].energy = MAX_energy;
+        ns[i].visited = 0;
+        ns[i].non = 0;
+        ns[i].region1 = 1;
+    }
+    for (i; i < R3; i++)
+    {
+        ns[i].id = i;
+        ns[i].x = rand() % 200 + 201;
+        ns[i].y = rand() % 200 + 1;
+        ns[i].type = rand() % 3 + 3;
+        ns[i].energy = MAX_energy;
+        ns[i].visited = 0;
+        ns[i].non = 0;
+        ns[i].region1 = 2;
+    }
+    for (i; i < R4; i++)
+    {
+        ns[i].id = i;
+        ns[i].x = rand() % 200 + 1;
+        ns[i].y = rand() % 200 + 201;
+        ns[i].type = rand() % 3 + 3;
+        ns[i].energy = MAX_energy;
+        ns[i].visited = 0;
+        ns[i].non = 0;
+        ns[i].region1 = 3;
+    }
+    for (i; i < S_NUM; i++)
+    {
+        ns[i].id = i;
+        ns[i].x = rand() % 200 + 201;
+        ns[i].y = rand() % 200 + 201;
+        ns[i].type = rand() % 3 + 3;
+        ns[i].energy = MAX_energy;
+        ns[i].visited = 0;
+        ns[i].non = 0;
+        ns[i].region1 = 4;
+    }
 }
 
 void neighbor_init()
@@ -409,185 +408,186 @@ int main()
 {
 	/*sensor initialization*/
 	srand((unsigned)time(NULL)); //random seed
-	fout << "AODV" << endl;
-	for( S_NUM ; S_NUM <= E_NUM ; S_NUM += 100){
+    fout << "AODV" << endl;
+    for( S_NUM ; S_NUM <= E_NUM ; S_NUM += 100){
 		cout << "sensors: " << S_NUM << endl;
 		fout << endl << "------------ Sensors " << S_NUM << " ------------" << endl;
-		for (int round = 0; round < round_number; round++)
-		{
-			// node_deployed();
-			special_node_deployed();
-			packet_init();
-			/*sink initialization*/
-			sink.id = SINKID;
-			/*neighbor_initialization*/
-			neighbor_init();
-			for (int b = 0; b < SINKBUFFER; b++)
-			{
-				sink.buffer[b].data = -1;
-				sink.buffer[b].dst = -1;
-				sink.buffer[b].src = -1;
-				sink.buffer[b].time = -1;
-			}
-			/*AODV路徑產生*/
-			AODV_path_assign();
-			/*traffic start*/
-			int bombing(0);
-			int b_region(0);
-			int die(0);//die=1就不跑迴圈
-			int t(1);
-			while (!die)
-			{
-				//fout << "time = " << t << endl;
-				int c = CheckEnergy();/*有一個節點沒電則等於死亡*/
-				if (c < SINKID)
-				{
-					avg_t += t;
-					//fout << "node " << c << " dead !" << endl;
-					die = 1;
-					//print_energy();
-					/*int e(0);
-					while (sink.buffer[e].data != -1)
-					{
-					e++;
-					}*/
-					//fout << t << endl;
-					//fout << "total = " << total << endl;
-					//fout << "drop = " << drop << endl;
-					//fout << "macdrop = " << macdrop << endl;
-					//fout << "sink 有" << e << endl; //total封包數,有些會找不到去sink的路徑
-					break;
-				}
-				if (freq_change_switch)
-				{
-					if (t % b_t <= s_t) //if in this time slot , then bombing.
-					{
-						if (b_region == 0)
-						{
-							b_region = rand() % 4 + 1; //bombing region 1~4 !
-						}
-						bombing = 1;
-					}
-					else //結束爆炸 ,調回參數
-					{
-						b_region = 0;
-						bombing = 0;
-					}
-				}
-				if (bombing)
-				{
-					/*爆炸區*/
-					if (t % bomb_f3 == 0)
-					{
-						for (int j = 0; j < S_NUM; j++)
-						{
-							if (ns[j].type == 3 && ns[j].region1 == b_region)
-							{
-								AODV_routing(j, t);
-							}
-						}
-					}
-					if (t % bomb_f4 == 0)
-					{
-						for (int j = 0; j < S_NUM; j++)
-						{
-							if (ns[j].type == 4 && ns[j].region1 == b_region)
-							{
-								AODV_routing(j, t);
-							}
-						}
-					}
-					if (t % bomb_f5 == 0)
-					{
-						for (int j = 0; j < S_NUM; j++)
-						{
-							if (ns[j].type == 5 && ns[j].region1 == b_region)
-							{
-								AODV_routing(j, t);
-							}
-						}
-					}
+        for (int rn = 0; rn < round_number; rn++)
+        {
+            // node_deployed();
+            special_node_deployed();
+            packet_init();
+            /*sink initialization*/
+            sink.id = SINKID;
+            /*neighbor_initialization*/
+            neighbor_init();
+            for (int b = 0; b < SINKBUFFER; b++)
+            {
+                sink.buffer[b].data = -1;
+                sink.buffer[b].dst = -1;
+                sink.buffer[b].src = -1;
+                sink.buffer[b].time = -1;
+            }
+            /*AODV路徑產生*/
+            AODV_path_assign();
+            /*traffic start*/
+            int bombing(0);
+            int b_region(0);
+            int die(0);//die=1就不跑迴圈
+            int t(1);
+            while (!die)
+            {
+                //fout << "time = " << t << endl;
+                int c = CheckEnergy();/*有一個節點沒電則等於死亡*/
+                if (c < SINKID)
+                {
+                    avg_t += t;
+                    //fout << "node " << c << " dead !" << endl;
+                    die = 1;
+                    //print_energy();
+                    /*int e(0);
+                    while (sink.buffer[e].data != -1)
+                    {
+                    e++;
+                    }*/
+                    //fout << t << endl;
+                    //fout << "total = " << total << endl;
+                    //fout << "drop = " << drop << endl;
+                    //fout << "macdrop = " << macdrop << endl;
+                    //fout << "sink 有" << e << endl; //total封包數,有些會找不到去sink的路徑
+                    break;
+                }
+                if (freq_change_switch)
+                {
+                    if (t % b_t <= s_t) //if in this time slot , then bombing.
+                    {
+                        if (b_region == 0)
+                        {
+                            b_region = rand() % 4 + 1; //bombing region 1~4 !
+                        }
+                        bombing = 1;
+                    }
+                    else //結束爆炸 ,調回參數
+                    {
+                        b_region = 0;
+                        bombing = 0;
+                    }
+                }
+                if (bombing)
+                {
+                    /*爆炸區*/
+                    if (t % bomb_f3 == 0)
+                    {
+                        for (int j = 0; j < S_NUM; j++)
+                        {
+                            if (ns[j].type == 3 && ns[j].region1 == b_region)
+                            {
+                                AODV_routing(j, t);
+                            }
+                        }
+                    }
+                    if (t % bomb_f4 == 0)
+                    {
+                        for (int j = 0; j < S_NUM; j++)
+                        {
+                            if (ns[j].type == 4 && ns[j].region1 == b_region)
+                            {
+                                AODV_routing(j, t);
+                            }
+                        }
+                    }
+                    if (t % bomb_f5 == 0)
+                    {
+                        for (int j = 0; j < S_NUM; j++)
+                        {
+                            if (ns[j].type == 5 && ns[j].region1 == b_region)
+                            {
+                                AODV_routing(j, t);
+                            }
+                        }
+                    }
 
-					/*非爆炸區*/
-					if (t % type3f == 0)
-					{
-						for (int j = 0; j < S_NUM; j++)
-						{
-							if (ns[j].type == 3 && ns[j].region1 != b_region)
-							{
-								AODV_routing(j, t);
-							}
-						}
-					}
-					if (t % type4f == 0)
-					{
-						for (int j = 0; j < S_NUM; j++)
-						{
-							if (ns[j].type == 4 && ns[j].region1 != b_region)
-							{
-								AODV_routing(j, t);
-							}
-						}
-					}
-					if (t % type5f == 0)
-					{
-						for (int j = 0; j < S_NUM; j++)
-						{
-							if (ns[j].type == 5 && ns[j].region1 != b_region)
-							{
-								AODV_routing(j, t);
-							}
-						}
-					}
-				}
-				else//regular trans
-				{
-					if (t % type3f == 0)
-					{
-						for (int j = 0; j < S_NUM; j++)
-						{
-							if (ns[j].type == 3) //CH need to sense
-							{
-								AODV_routing(j, t);
-							}
-						}
-					}
-					if (t % type4f == 0)
-					{
-						for (int j = 0; j < S_NUM; j++)
-						{
-							if (ns[j].type == 4) //CH need to sense
-							{
-								AODV_routing(j, t);
-							}
-						}
-					}
-					if (t % type5f == 0)
-					{
-						for (int j = 0; j < S_NUM; j++)
-						{
-							if (ns[j].type == 5) //CH need to sense
-							{
-								AODV_routing(j, t);
-							}
-						}
-					}
-				}
+                    /*非爆炸區*/
+                    if (t % type3f == 0)
+                    {
+                        for (int j = 0; j < S_NUM; j++)
+                        {
+                            if (ns[j].type == 3 && ns[j].region1 != b_region)
+                            {
+                                AODV_routing(j, t);
+                            }
+                        }
+                    }
+                    if (t % type4f == 0)
+                    {
+                        for (int j = 0; j < S_NUM; j++)
+                        {
+                            if (ns[j].type == 4 && ns[j].region1 != b_region)
+                            {
+                                AODV_routing(j, t);
+                            }
+                        }
+                    }
+                    if (t % type5f == 0)
+                    {
+                        for (int j = 0; j < S_NUM; j++)
+                        {
+                            if (ns[j].type == 5 && ns[j].region1 != b_region)
+                            {
+                                AODV_routing(j, t);
+                            }
+                        }
+                    }
+                }
+                else//regular trans
+                {
+                    if (t % type3f == 0)
+                    {
+                        for (int j = 0; j < S_NUM; j++)
+                        {
+                            if (ns[j].type == 3) //CH need to sense
+                            {
+                                AODV_routing(j, t);
+                            }
+                        }
+                    }
+                    if (t % type4f == 0)
+                    {
+                        for (int j = 0; j < S_NUM; j++)
+                        {
+                            if (ns[j].type == 4) //CH need to sense
+                            {
+                                AODV_routing(j, t);
+                            }
+                        }
+                    }
+                    if (t % type5f == 0)
+                    {
+                        for (int j = 0; j < S_NUM; j++)
+                        {
+                            if (ns[j].type == 5) //CH need to sense
+                            {
+                                AODV_routing(j, t);
+                            }
+                        }
+                    }
+                }
 
-				t++;
-			}
-			cout << round+1 << endl;
-		}
-		total /= round_number;
-		macdrop /= round_number;
-		drop /= round_number;
-		avg_t /= round_number;
-		fout << "avg_lifetime : " << avg_t << endl;
-		fout << "avg_total : " << total << endl;
-		fout << "avg_macdrop : " << macdrop << endl;
-		fout << "avg_drop : " << drop << endl;
-		fout << "avg_PLR : " << (drop + macdrop) / total << endl;
-	}
+                t++;
+            }
+            cout << rn+1 << endl;
+        }
+        total /= round_number;
+        avg_t /= round_number;
+        drop /= round_number;
+        macdrop /= round_number;
+        double PLR = (drop + macdrop)/ total;
+        fout << "avg_lifetime : " << avg_t << endl;
+        fout << "avg_total : " << total << endl;
+        fout << "avg_drop : " << drop << endl;
+        fout << "avg_macdrop : " << macdrop << endl;
+        fout << "avg_PLR : " << PLR << endl;
+    }
 	return 0;
 }
 
